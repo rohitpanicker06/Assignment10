b11596a6501d7ca54af343cc33fdbba95ff6ffcd
rohitpanicker06@gmail.com <rohitpanicker06@gmail.com>
2023-03-31 22:15:35 -0400

    Initial container and structure for assignment 10

---
+++ new-weather-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import ContainerForDisplayingWeekWeather from  './ContainerForDisplayingWeekWeather';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContainerForDisplayingWeekWeather />
+      </div>
+    );
+  }
+}
+
+
+export default App;
